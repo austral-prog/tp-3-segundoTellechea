30ed8b66e514078945eae1b2b25d1bb5724c70d7
segundoTellechea <stellechea@mail.austral.edu.ar>
2025-04-11 11:30:17 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,8 +1,3 @@
-def slice_simple():
-    texto = "Awesome"
-    primeras = texto[0:3].lower()
-    medio = texto[2:5].lower()
-    combinado = texto[0:4].lower() + texto[-3:].lower()
-    print(primeras)
-    print(medio)
-    print(combinado)
+def slice_advanced():
+	texto=input("Ingrese un texto: ")
+	print(texto[4::2])
